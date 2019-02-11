44aa8fe5c4a2158e69636ef39b93a3c55a319ae5
Bikash Kumar Bhandari <30164638+bkb3@users.noreply.github.com>
2019-02-11 21:14:55 +1300

    rewrite in pandas

--- avoidance2/score.py
+++ avoidance2/score.py
@@ -1,14 +1,14 @@
-import argparse
-import sys
 import os
-import pandas as pd
-import numpy as np
+import sys
 import time
-from sklearn.externals import joblib
-from hmmlearn import hmm
+import pickle
+import argparse
+import numpy as np
+import pandas as pd
 from libs import functions
-from itertools import zip_longest
-#from threading import Thread
+
+
+
 
 
 def check_arg(args=None):
@@ -17,16 +17,21 @@ def check_arg(args=None):
                         help='Sequences in CSV. FASTA support coming soon!',
                         required='True')
     parser.add_argument('-m', '--model',
-                        help='HMM file in .pkl format.',
+                        help='Trained model file in .pkl format.',
                         required='True')
-    parser.add_argument('-v',
-                        help='Verbosity. Leave blank for no verbosity.',
-                        action="store_true")
+    parser.add_argument('-b', '--background',
+                        help='Background model file in .pkl format.',
+                        required='True')
+    parser.add_argument('-o', '--output',
+                        help='Output file name',
+                        default='scores')
+
 
     results = parser.parse_args(args)
     return (results.file,
             results.model,
-            results.v)
+            results.background,
+            results.output)
 
 
 
@@ -38,32 +43,57 @@ def main():
         pass
     else:
         os.makedirs(os.path.join(os.getcwd(),'results','scores',''))
+    
+    #mask NaNs with mean
+    for i in range(prob_data.shape[1]-1):
+        prob_data[i].fillna(prob_data['mean'], inplace=True)
+    for i in range(back_prob_data.shape[1]-1):
+        back_prob_data[i].fillna(back_prob_data['mean'], inplace=True)
+    
+    sequence_df = pd.read_csv(f,skiprows=1,header=None)
+    sequence_score=pd.DataFrame(columns=['scores','scores_per_codon'],index=list(range(len(sequence_df))))
+    length_to_score = max([len(sequence_df[0][i]) for i in range(len(sequence_df))]) #score for full length
+    for seq in range(len(sequence_df)):
+        sequence = sequence_df[0][seq].lower()
+        length = functions.sequence_length(sequence) if len(sequence)<length_to_score\
+                 else length_to_score
+        codons = functions.splitter(sequence,length)
+        scores_df = pd.DataFrame(columns=['scores'],index=codons)
+        back_scores_df = pd.DataFrame(columns=['scores'],index=codons)
+        for i in range(len(codons)):
 
 
-    df = pd.read_csv(f,skiprows=1,header=None)
-    codon_list = functions.codon_splitter(df[0],3,v)
-    lengths = [len(codon_list[index]) for index in range(len(codon_list))]
-    codon_to_num = functions.create_mapping(codon_list,'encode')
-    num_to_codon = functions.create_mapping(codon_list,'decode')
-    X = functions.convert_to_hmm_data(codon_list,codon_to_num,v)
-    base_model = joblib.load(m)
-    scores = functions.calc_scores(base_model,X,lengths,v)
+            try:
+                if i < len(prob_data.columns)-1: #1 for average
+                    scores_df.loc[codons[i],'scores'] = np.log2(prob_data.loc[codons[i],i])[0]
+                    back_scores_df.loc[codons[i],'scores'] = np.log2(back_prob_data.loc[codons[i],i])[0]
+                else:
+                    scores_df.loc[codons[i],'scores'] = np.log2(prob_data.loc[codons[i],'mean'])[0]
+                    back_scores_df.loc[codons[i],'scores'] = np.log2(back_prob_data.loc[codons[i],'mean'])[0]
 
 
-    scores_all = pd.DataFrame(list(zip_longest(df[0],scores )),\
-                          columns=['sequences','scores'])
-    #functions.helicopter('stop')
-    print('Exporting scores to csv..')
-    scores_all.to_csv(mypath+'scores_'+time.strftime("%Y%m%d-%H%M%S") + '.csv'
+            except RuntimeWarning: #catch for log zero error
+                scores_df.loc[codons[i],'scores'] = np.log2(prob_data.loc[codons[i],'mean'])[0]
+                back_scores_df.loc[codons[i],'scores'] = np.log2(back_prob_data.loc[codons[i],'mean'])[0]
+
+
+        sequence_score.loc[seq,'scores'] = scores_df.sum()[0] - back_scores_df.sum()[0]          
+        sequence_score.loc[seq,'scores_per_codon'] = scores_df.mean()[0] - back_scores_df.mean()[0] 
+        functions.progress(seq,len(sequence_df))
+
+
+    print('\nExporting results..')    
+    sequence_df['scores'] = sequence_score['scores']
+    sequence_df['scores_per_codon'] = sequence_score['scores_per_codon']
+    sequence_df.to_csv(mypath+'_'+o+'_'+time.strftime("%Y%m%d-%H%M%S") + '.csv'
                       ,sep=',', encoding='utf-8', index=False)
 
 
-
 if __name__ == '__main__':
-    f,m,v= check_arg(sys.argv[1:])
-    #if v == False:
-    #    Thread(target=functions.helicopter('start')).start
-    #    Thread(target=main()).start
-
+    f,m,b,o= check_arg(sys.argv[1:])
+    with open(m, "rb") as model_file:
+        prob_data = pickle.load(model_file)
+    with open(b, "rb") as model_file:
+        back_prob_data = pickle.load(model_file)
     main()
 
