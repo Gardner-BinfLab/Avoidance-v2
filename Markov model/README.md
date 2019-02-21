409cb4ffb635cf51c8aa17a07b9457075ca68e83
bikash <mk.bikash@gmail.com>
2019-02-22 10:45:50 +1300

    resync

---
+++ Markov model/README.md
@@ -0,0 +1,77 @@
+# Markov model
+### Changelog
+ - Major rewrite and refactoring. 
+ - We now use the highly scalable Pandas dataframes instead of plain numpy arrays.
+ - Useless dependencies removed.
+
+
+
+# Install dependencies 
+Requires ~`Python3.6+`~`Python3.4+`
+
+This command installs the required dependencies.
+
+`pip3 install --user scipy numpy pandas `
+
+
+# Manual
+  - `-h` gives possible options.
+  - An example model is included and some training sequences are in test folder.
+
+
+# Train
+Train takes a list of sequences in .csv or .fasta and builds the model. The built model is 
+exported as a python pickle (.pkl) file in 'results/model' subdirectory.
+We train for 32 codons (96 nucleotides) by default but you can pass the length
+in codons or just use flag `-a` to train over full length (not recommended).
+
+
+```console
+$ python3 train.py -f /directory/train_set_.csv 
+Reading codons..
+[===============                                   ] 30% (410/1333)
+Stop codon encountered somewhere before the last position.
+[==================================================] 100% (1333/1333)Finished!
+1  sequence(s) were skipped because we encountered stop codons.
+
+Training started. It may take a while..
+
+Zeroth order
+[==================================================] 100% (31/31)Finished!
+
+Exporting model data to file..
+
+```
+
+# Scores
+To score the sequences, you need to provide foreground and background model.
+They can be built by training (above).
+
+
+Example use case:
+```console
+$ python3 score.py -f /dir/test.csv -m /dir/foreground.pkl -b /dir/backgnd.pkl
+[===============                                   ] 30% (410/1334)
+Stop codons encountered before the last position for sequence :  411
+[================================================= ] 99% (1333/1334)
+Exporting results..
+
+```
+
+
+
+# Generate
+This generates sequences from given sequence. You have to specify the sequence,
+model and number of sequences to generate.
+
+
+Example use case:
+```console
+$ python3 seqgen.py -s 
+agtgtttgtgtctgcaatcccaagtttgtttgcgctgaaatatgcgatgctcaatgttatgatctgcgtactaagccgcagatcatagtgggaact 
+-m example_model.pkl -n 200 
+
+Generating sequences..
+[=========================================================] 100% Time:  0:00:00
+Exporting scores to csv..
+```
---
+++ Markov model/generate.py
@@ -0,0 +1,94 @@
+import sys
+import os
+import argparse
+import pickle
+import time
+import pandas as pd
+from numpy.random import choice
+from libs import functions
+from libs import data
+
+
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='This attempts to generate synonymous sequences.')
+    parser.add_argument('-s', '--sequence',
+                        help='Sequence to optimize',
+                        required='True')
+    parser.add_argument('-m', '--model',
+                        help='model file in .pkl format.',
+                        required='True')
+    parser.add_argument('-n',type=int, 
+                        help='Number of synonymous sequences to generate.',
+                        default=1000)
+    parser.add_argument('-o', '--output',
+                        help='Output file name.',
+                        default = 'synonymous_seq')
+
+    
+    results = parser.parse_args(args)
+    return (results.sequence,
+            results.model,
+            results.n,
+            results.output)
+
+    
+def main():
+    
+    #fix seed
+    np.random.seed(12345)
+    
+    mypath = os.path.join(os.getcwd(),'results','synonymous_sequences','')
+    if os.path.exists(mypath)==True:
+        pass
+    else:
+        os.makedirs(os.path.join(os.getcwd(),'results','synonymous_sequences',''))
+    
+    
+    
+    input_sequence = s.lower()
+    syn_sequences = []
+    for seq in range(n):
+        length = functions.sequence_length(input_sequence)
+        codons = functions.splitter(input_sequence,length)
+        chain = ''
+
+
+
+        for i in range(len(codons)):
+            possible_codons = data.aa2codon[data.codon2aa[codons[i]]]
+            try:
+                probs_of_codons_for_emission = prob_data.loc[possible_codons,i]
+                if probs_of_codons_for_emission.dropna().empty == True:
+                    try:
+                        probs_of_codons_for_emission = prob_data.loc[possible_codons,'codon_prob']
+                    except (IndexError,RuntimeWarning): #check if we are at the end
+                        probs_of_codons_for_emission = prob_data.loc[possible_codons,'codon_prob']
+            except (KeyError,RuntimeWarning): #for positions longer then in the probability table
+                probs_of_codons_for_emission = prob_data.loc[possible_codons,'codon_prob']
+                
+                
+
+            scores_sum = sum(probs_of_codons_for_emission)
+            chain+=choice(possible_codons, p = [float(i)/scores_sum\
+                                                for i in probs_of_codons_for_emission])
+
+            #for random sequences
+            #chain+=choice(prob_data.index,p=prob_data.loc[:,i])
+
+        syn_sequences.append(chain.upper())
+        functions.progress(seq,n)
+    
+    print("\nExporting sequences...")
+    dataframe = pd.DataFrame(syn_sequences,columns=['synonymous_sequence'])
+    filename = mypath+'_' + o +'_'+time.strftime("%Y%m%d-%H%M%S")+'.csv'
+    dataframe.to_csv(filename,sep=',', encoding='utf-8', index=False) 
+        
+
+        
+if __name__ == '__main__':
+    s,m,n,o= check_arg(sys.argv[1:])
+    with open(m, "rb") as model_file:
+        prob_data = pickle.load(model_file)
+    main()
+
+   
