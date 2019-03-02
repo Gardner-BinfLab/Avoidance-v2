8b823f83bd3e5fb7868dda7f83a44c901e2cf86f
lcscs12345 <lcscs12345@gmail.com>
2019-03-02 16:34:50 +1300

    Updated basepair.py to Python 3.7

--- Random_forest/basepair.py
+++ Random_forest/basepair.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 from builtins import range
 import sys
 import math
 import argparse
 from re import sub
+import collections
+import pandas as pd
 from Bio import SeqIO
 import RNA
 import forgi.graph.bulge_graph as cgb
@@ -23,27 +24,49 @@ def basepair(seqfile, win, step):
     for seq_record in SeqIO.parse(seqfile, "fasta"):
         seq = str(seq_record.seq)
         id = str(seq_record.id)
-        bg = cgb.BulgeGraph()
         for subseq in chunks(seq, win, step):
             dotbracket = RNA.fold(subseq)[0]
+            bg = cgb.BulgeGraph.from_dotbracket(dotbracket)
             bg.from_dotbracket(dotbracket)
             for s in bg.stem_iterator():
                 for i in range(bg.stem_length(s)):
-                    l=int((bg.defines[s][0]+i)%3)
-                    r=int((bg.defines[s][3]-i)%3)
-                    print(id, l, r, sep="\t")
+                    l=(bg.defines[s][0]+i)%3
+                    r=(bg.defines[s][3]-i)%3
+                    yield id, l, r
+
+
+def count(seqfile, win, step):
+    g = basepair(seqfile, win, step)
+    for i in g:
+        c = i[0]
+        if (i[1]==0 and i[2]==2) or (i[1]==2 and i[2]==0) or (i[1]==1 and i[2]==1):
+            yield c,'c1'
+        elif (i[1]==0 and i[2]==1) or (i[1]==1 and i[2]==0) or (i[1]==2 and i[2]==2):
+            yield c,'c2'
+        elif (i[1]==1 and i[2]==2) or (i[1]==2 and i[2]==1) or (i[1]==3 and i[2]==3):
+            yield c,'c3'
     return
 
 
+
 def main():
-    basepair(args.i,args.w,args.s)
+    counter = collections.Counter(count(args.i, args.w, args.s))
+    df = pd.DataFrame.from_dict(counter, orient='index').reset_index()
+    df = pd.concat([df['index'].apply(pd.Series), df[0]], axis = 1)
+    df.columns = ['Accession', 'basepair', 'counts']
+    df = df.pivot(index='Accession', columns='basepair', values='counts')
+    df['Total']=df['c1']+df['c2']+df['c3']
+    df['C1']=df['c1']/df['Total']*100
+    df['C2']=df['c2']/df['Total']*100
+    df['C3']=df['c3']/df['Total']*100
+    df.to_csv(r'basepair.out',sep='\t')
     return
 
 
 if __name__ == '__main__':
-    Argument_Parser=argparse.ArgumentParser(prog="basepair.py")
-    Argument_Parser.add_argument('-i',type=str,metavar='STR',help="a FASTA file of CDS",required=True)
-    Argument_Parser.add_argument('-w',type=int,metavar='INT',help="Window size (nt)",required=True)
-    Argument_Parser.add_argument('-s',type=int,metavar='INT',help="Step (nt)",required=True)
-    args=Argument_Parser.parse_args()
+    parser=argparse.ArgumentParser(description="Calculating RNA basepair classes")
+    parser.add_argument('-i',type=str,metavar='STR',help="a FASTA file of CDS",required=True)
+    parser.add_argument('-w',type=int,metavar='INT',help="Window size (nt)",required=True)
+    parser.add_argument('-s',type=int,metavar='INT',help="Step (nt)",required=True)
+    args=parser.parse_args()
     main()
