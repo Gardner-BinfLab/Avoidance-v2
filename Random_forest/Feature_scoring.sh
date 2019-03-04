ca1bc1911a7c1d53a356124d887f263641c75620
Chun Shen Lim <lcscs12345@gmail.com>
2019-03-05 11:58:14 +1300

    Updated Feature_scoring.sh

--- Random_forest/Feature_scoring.sh
+++ Random_forest/Feature_scoring.sh
@@ -35,7 +35,7 @@ echo "Calculating Open Energies for Accessibility(1:30)..."
 echo ""
 mkdir rnaplfold
 cd rnaplfold
-time awk 'BEGIN{RS=">"}NR>1{sub("\n","\t"); gsub("\n",""); print RS$0}' ${CDS} \
+time awk 'BEGIN{RS=">"}NR>1{sub("\n","\t"); gsub("\n",""); print RS$0}' ${DIR}/${CDS} \
 | awk -v U=${UTR5} '{print $1 "\n" U $2}' \
 | RNAplfold -W 210 -u 210 -O
 for i in *_openen; do \
@@ -65,19 +65,7 @@ echo ""
 echo "....................................................................................."
 echo "Calculating basepair classes..."
 echo ""
-python ${SRC}/basepair.py -i ${CDS} -w 60 -s 6 | gzip > basepair.raw.gz
-zcat basepair.raw.gz \
-| sed 's/\t/,/2;s/0,0/3/;s/0,1/2/;s/0,2/1/;s/1,0/2/;s/1,1/1/;s/1,2/3/;s/2,0/1/;s/2,1/3/;s/2,2/2/' \
-| sort | uniq -c \
-| awk 'BEGIN{OFS="\t"}{print $2,$3,$1}' > basepair.txt
-paste \
-<(awk '$2==1 {print $1 "\t" $3}' basepair.txt) \
-<(awk '$2==2 {print $3}' basepair.txt) \
-<(awk '$2==3 {print $3}' basepair.txt) \
-| join -t$'\t' \
-<(sort -k1,1 -) \
-<(awk '{a[$1] += $3} END{for (i in a) print i "\t" a[i]}' basepair.txt | sort -k1,1) \
-| sed '1i Accession\tc1\tc2\tc3\tTotal_c' > basepair.out
+time python ${SRC}/basepair.py -i ${CDS} -w 60 -s 6 | gzip > basepair.out
 
 echo ""
 echo "....................................................................................."
@@ -85,7 +73,8 @@ echo "Calculating Codon Usage Indices..."
 time codonw ${CDS} codonw.out codonw.blk \
 -cai -fop -cbi -enc -gc -gc3s -sil_base \
 -nomenu -silent
-awk 'BEGIN{OFS="\t"}{sub("title","Accession"); print $1,$2,$3,$4,$5,$6,$9,$10,$11}' codonw.out > codonw.out2
+awk 'BEGIN{OFS="\t"}{sub("title","Accession"); print $1,$2,$3,$4,$5,$6,$9,$10,$11}' codonw.out \
+| sed 's/*//g' > codonw.out2
 mv codonw.out2 codonw.out
 
 echo ""
@@ -103,30 +92,6 @@ echo ""
 echo "....................................................................................."
 echo "Calculating Minimum Free Energies for Avoidance of mRNA:ncRNA Intereactions(1:30)..."
 echo ""
-mkdir ${DIR}/avoidance_scores
-cd ${DIR}/avoidance_scores
-awk 'BEGIN{RS=">"}NR>1{sub("\n","\t"); gsub("\n",""); print RS$0}' ${CDS} \
-| awk '{print $1 "\n" substr($NF,1,30)}' > w1.30.fa
-time python ${SRC}/RNAup_avoidance_calculator.py \
--mrna w1.30.fa \
--ncrna ${SRC}/data/ncrna.ril.fa \
--parallel ${THREADS}
-for i in *.result; do \
-  sequence=$(echo ${i%.*})
-  avoidance_mfe=$(cat ${i%.*}.result \
-  | awk 'match($0,/.*\s\(([-]*[0-9]+.[0-9]+)\s=\s.*/,m){avoid+=m[1]}END{print avoid}')
-  echo -e $sequence"\t"$avoidance_mfe
-done \
-| sed '1i Accession\tAvoidance' > ${DIR}/avoidance.out
-#avoidance for each ncRNA
-for i in *.result; do \
-  paste \
-  <(echo ${i%.*}) \
-  <(awk 'match($0,/.*\s\(([-]*[0-9]+.[0-9]+)\s=\s.*/,m){print m[1]}' ${i} | transpose -t)
-done \
-| cat \
-<(grep ">" ${SRC}/data/ncrna.ril.fa | sed 's/>//;s/-/_/g' | transpose -t | sed 's/^/Accession\t/') - \
-> ${DIR}/avoidance.ncrna.out
-cd ${DIR}
+python avoidance.py -m ${CDS} -n ${SRC}/data/ncrna.ril.fa
 
-#rm -r avoidance_scores rnaplfold ${CDS%.*}_*.txt *.blk basepair.txt
+rm -r rnaplfold ${CDS%.*}_*.txt *.blk
