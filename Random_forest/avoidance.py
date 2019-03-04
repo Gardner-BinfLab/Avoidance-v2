89b8157f72b8d1af10e72341a07ea2ed0ddebd85
lcscs12345 <lcscs12345@gmail.com>
2019-03-05 10:26:26 +1300

    Updated avoidance.py

--- Random_forest/avoidance.py
+++ Random_forest/avoidance.py
@@ -7,8 +7,6 @@ import itertools
 import subprocess
 import pandas as pd
 import multiprocessing
-from itertools import cycle
-from libs import functions
 from datetime import datetime
 from multiprocessing import Pool
 from subprocess import run, PIPE
@@ -56,6 +54,33 @@ def check_arg(args=None):
 
 
 
+def progress(iteration, total):   
+    bars_string = int(float(iteration) / float(total) * 50.)
+    sys.stdout.write(
+        "\r|%-50s| %d%% (%s/%s)" % (
+            '█'*bars_string+ "░" * (50 - bars_string), float(iteration) / float(total) * 100,
+            iteration,
+            total
+        ) 
+    )
+    sys.stdout.flush()
+    if iteration == total:
+        print(' Completed!') 
+
+
+def fasta_to_dataframe(f):
+    fasta_df = pd.read_csv(f,sep='>', lineterminator='>',header=None)
+    fasta_df[['accession','sequence']]=fasta_df[0].str.split('\n', 1, expand=True)
+    fasta_df['accession'] = '>'+fasta_df['accession']
+    fasta_df['sequence'] = fasta_df['sequence'].replace('\n','', regex=True)
+    fasta_df.drop(0, axis=1, inplace=True)
+    fasta_df.set_index('accession',inplace=True)
+    fasta_df = fasta_df[fasta_df.sequence != '']
+    final_df = fasta_df.dropna()
+    return final_df
+
+
+
 def interaction_calc(seq):
     proc = run(['RNAup', '-b', '-o', '--interaction_first'], stdout=PIPE,stderr=subprocess.DEVNULL,
                input=str.encode(seq)) #input is a stdin object so encode input str
@@ -66,13 +91,13 @@ def interaction_calc(seq):
 def main():
     base,ext = os.path.splitext(m)
     if ext.lower() in ('.fasta', '.fa', '.fas', '.fna'):
-        mrna = functions.fasta_to_dataframe(m)
+        mrna = fasta_to_dataframe(m)
     else:
         mrna = pd.read_csv(m, skiprows=1, header=None)
         
     base,ext = os.path.splitext(n)
     if ext.lower() in ('.fasta', '.fa', '.fas', '.fna'):
-        ncrna = functions.fasta_to_dataframe(n)
+        ncrna = fasta_to_dataframe(n)
     else:
         ncrna = pd.read_csv(n, skiprows=1, header=None)
 
@@ -80,8 +105,8 @@ def main():
     
     print('\nAssigning mRNA:ncRNA interactions...')
     startTime = datetime.now()
-    mrna['mrna_seq'] = '>' + mrna[1].map(str) + '\n' + mrna[0].map(str).str[:length] + '\n'
-    ncrna['ncrna_seq'] = '>' + ncrna[1].map(str) + '\n' + ncrna[0].map(str) + '\n'
+    mrna['mrna_seq'] = mrna.index + '\n' + mrna['sequence'].map(str).str[:length] + '\n'
+    ncrna['ncrna_seq'] = ncrna.index + ':break' + '\n' + ncrna['sequence'].map(str) + '\n'
     mrna_seq = [rows['mrna_seq'] for index,rows in mrna.iterrows()]   
     ncrna_seq = [rows['ncrna_seq'] for index,rows in ncrna.iterrows()]
     index = pd.MultiIndex.from_product([mrna_seq, ncrna_seq], names = ['mrna', 'ncrna'])
@@ -94,33 +119,21 @@ def main():
     groups = df.shape[0]
     my_pool = Pool(p)
     interactions = []
-    functions.progress(0,groups)
+    progress(0,groups)
     for i in my_pool.imap_unordered(interaction_calc, df['interaction_first'], chunksize=int(groups/p)):
         interactions.append(i)
-        functions.progress(len(interactions), groups)
+        progress(len(interactions), groups)
         
     my_pool.close()
     my_pool.join()
 
     #parsing RNAup output
-    seq_id = pd.Series(interactions).str.extractall(r'(>[\S]+)')[0].str.replace('>', '', regex=True).to_frame()
-    mrna_id = (seq_id.loc[pd.IndexSlice[:, 0], :]).reset_index().set_index('level_0')
-    ncrna_id = (seq_id.loc[pd.IndexSlice[:, 1:], :]).reset_index().set_index('level_0')
-    binding_energy = pd.Series(interactions).str.extractall(r'(\(-[0-9]+\.[0-9]+)')[0].str.replace('(', '', regex=True).to_frame().reset_index().set_index('level_0')
-    
-    d = pd.concat([mrna_id.iloc[:,[1]], ncrna_id.iloc[:,[1]]], axis=1)
-    match = cycle(list(range(len(ncrna))))
-    d['match'] = [next(match) for i in range(len(d))]
-    d = d.reset_index()
-    d['level_0']=d['level_0'].astype(int)
-
-    binding_energy = binding_energy.reset_index()
-    binding_energy['level_0']=binding_energy['level_0'].astype(int)
-    binding_energy['match']=binding_energy['match'].astype(int)
-    
-    d = pd.merge(binding_energy.reset_index(), d.reset_index(), on=['level_0', 'match'])
-    d = d.iloc[:,[5,6,3]]
-    d.columns = ['Accession', 'ncRNA', 'Binding_energy']
+    ncrna_id = pd.Series(interactions).str.extractall(r'(>[\S]+:break)')[0].str.replace('[>:break]', '', regex=True).to_frame().reset_index()
+    mrna_id = pd.Series(interactions).str.extractall(r'(>[\S]+)')[0].str.replace('[>]', '', regex=True).to_frame().loc[pd.IndexSlice[:, 0], :].reset_index()
+    binding_energy = pd.Series(interactions).str.extractall(r'(\(-[0-9]+\.[0-9]+)')[0].str.replace('(', '', regex=True).to_frame().reset_index()
+    d = pd.merge(ncrna_id, binding_energy, on=['level_0','match'])
+    d = pd.merge(mrna_id, d, on='level_0').iloc[:, [2,4,5]]
+    d.columns = ['Accession', 'ncRNA', 'binding_energy']
 
     filename = o + '.out'
     d.to_csv(filename, index=False, sep='\t', encoding='utf-8')
