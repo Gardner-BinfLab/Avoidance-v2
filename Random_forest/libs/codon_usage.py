e379de7a3d6f3561638073a7a9f9756b056c1bf1
lcscs12345 <lcscs12345@gmail.com>
2019-03-09 17:32:12 +1300

    Added cai_cost.py

---
+++ Random_forest/libs/codon_usage.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+#
+
+import math
+from Bio.SeqUtils.CodonUsageIndices import SharpEcoliIndex
+from Bio import Data, SeqIO
+
+"""Adapted from https://github.com/Benjamin-Lee/biopython/blob/master/Bio/SeqUtils/CodonUsage.py
+Correct CAI Implementation and Multiple Genetic Codes:
+Fixes an incorrect implementation of the CAI formula as defined by Sharp and Li. They state "that if a certain codon is never used in the reference set then the CAI for any other gene in which that codon appears becomes zero. To overcome this problem we assign a value of 0.5 to any [codon] that would otherwise be zero." Also adds support for multiple genetic codes.
+"""
+
+class CodonAdaptationIndex(object):
+    """A codon adaptation index (CAI) implementation.
+    Implements the codon adaptation index (CAI) described by Sharp and
+    Li (Nucleic Acids Res. 1987 Feb 11;15(3):1281-95).
+    """
+
+    def __init__(self):
+        self.rscu = {}
+        self.genetic_code = 1
+        self.index = {}
+        self.codon_count = {}
+        self.CodonsDict = {'TTT': 0, 'TTC': 0, 'TTA': 0, 'TTG': 0, 'CTT': 0,
+        'CTC': 0, 'CTA': 0, 'CTG': 0, 'ATT': 0, 'ATC': 0,
+        'ATA': 0, 'ATG': 0, 'GTT': 0, 'GTC': 0, 'GTA': 0,
+        'GTG': 0, 'TAT': 0, 'TAC': 0, 'TAA': 0, 'TAG': 0,
+        'CAT': 0, 'CAC': 0, 'CAA': 0, 'CAG': 0, 'AAT': 0,
+        'AAC': 0, 'AAA': 0, 'AAG': 0, 'GAT': 0, 'GAC': 0,
+        'GAA': 0, 'GAG': 0, 'TCT': 0, 'TCC': 0, 'TCA': 0,
+        'TCG': 0, 'CCT': 0, 'CCC': 0, 'CCA': 0, 'CCG': 0,
+        'ACT': 0, 'ACC': 0, 'ACA': 0, 'ACG': 0, 'GCT': 0,
+        'GCC': 0, 'GCA': 0, 'GCG': 0, 'TGT': 0, 'TGC': 0,
+        'TGA': 0, 'TGG': 0, 'CGT': 0, 'CGC': 0, 'CGA': 0,
+        'CGG': 0, 'AGT': 0, 'AGC': 0, 'AGA': 0, 'AGG': 0,
+        'GGT': 0, 'GGC': 0, 'GGA': 0, 'GGG': 0}
+
+        # this dictionary shows which codons encode the same AA
+        self.SynonymousCodons = {}
+
+    # use this method with predefined CAI index
+    def set_cai_index(self, index):
+        """Sets up an index to be used when calculating CAI for a gene.
+        Just pass a dictionary similar to the SharpEcoliIndex in the
+        CodonUsageIndices module.
+        """
+        self.index = index
+
+    def generate_rscu(self, fasta_file):
+        """Create an RSCU (Relative Synonymous Codon Usage) table from a FASTA file of CDS sequences.
+        Takes a location of a Fasta file containing CDS sequences
+        (which must all have a whole number of codons) and generates an RSCU dictionary.
+        """
+        # first make sure we're not overwriting an existing index:
+        if self.index or self.codon_count or self.rscu:
+            raise ValueError("an index has already been set or a codon count has been done. cannot overwrite either.")
+
+        # sets up the genetic code dictionary (which is defualted to 1)
+        for key, value in Data.CodonTable.unambiguous_dna_by_id[self.genetic_code].forward_table.items():
+            try:
+                self.SynonymousCodons[value].append(key)
+            except KeyError:
+                self.SynonymousCodons[value] = [key]
+
+        # count codon occurrences in the file.
+        self._count_codons(fasta_file)
+
+        # now to calculate the index we first need to sum the number of times
+        # synonymous codons were used all together.
+        for aa in self.SynonymousCodons:
+            total = 0.0
+            rscu_for_amino_acid = []  # RSCU values are CodonCount/((1/num of synonymous codons) * sum of all synonymous codons)
+            codons = self.SynonymousCodons[aa]
+
+            for codon in codons:
+                total += self.codon_count[codon]
+
+            # calculate the rscu value for each of the codons
+            for codon in codons:
+                denominator = float(total) / len(codons)
+                self.rscu[codon] = self.codon_count[codon] / denominator
+
+    def generate_index(self, *args):
+        """Generate a codon usage index from a the instance's RSCU.
+        Takes a the instance's RSCU dictionary and generates a codon
+        usage index.
+        """
+        # first make sure an RSCU table is set. Optionally accepts a FASTA file location for backwards compatibility.
+        if not self.rscu and not self.index:
+            try:
+                self.generate_rscu(args[0])
+            except IndexError:
+                raise IndexError("No RSCU table or index set and no FASTA file location passed")
+
+        if self.genetic_code != 1:
+            change_tranlation_table(self.genetic_code)
+
+        for aa in self.SynonymousCodons:
+            codons = self.SynonymousCodons[aa]
+
+            # now generate the index W=rscui/rscumax:
+            rscu_max = max([self.rscu[codon] for codon in codons])
+            for codon in codons:
+                self.index[codon] = self.rscu[codon] / float(rscu_max) if self.rscu[codon] != 0 else 0.5 / float(rscu_max)  # uses an RSCU value of 0.5 if the codon is not used in the reference set
+
+    def cai_for_gene(self, dna_sequence):
+        """Calculate the CAI (float) for the provided DNA sequence (string).
+        This method uses the Index (either the one you set or the one you generated)
+        and returns the CAI for the DNA sequence.
+        """
+        cai_value, cai_length = 0, 0
+        # if an RSCU table is given but no index is set or generated, generates an index
+        if not self.index and self.rscu:
+            self.generate_index()
+
+        # if no index is set or generated, the default SharpEcoliIndex will be used.
+        if not self.index and not self.rscu:
+            self.set_cai_index(SharpEcoliIndex)
+            #print("No index or RSCU set... using default SharpEcoliIndex!")
+
+        if dna_sequence.islower():
+            dna_sequence = dna_sequence.upper()
+
+        for i in range(0, len(dna_sequence), 3):
+            codon = dna_sequence[i:i + 3]
+            if codon in self.index:
+                if codon not in ['ATG', 'TGG']:  # these two codons are always one; exclude them
+                    cai_value += math.log(self.index[codon])
+                    cai_length += 1
+            elif codon not in ['TGA', 'TAA', 'TAG']:  # some indices may not include stop codons
+                raise TypeError("illegal codon in sequence: %s.\n%s" % (codon, self.index))
+
+        return math.exp(cai_value / (cai_length - 1.0))
+
+    def _count_codons(self, fasta_file):
+        with open(fasta_file, 'r') as handle:
+
+            # make the codon dictionary local
+            self.codon_count = self.CodonsDict.copy()
+
+            # iterate over sequence and count all the codons in the FastaFile.
+            for cur_record in SeqIO.parse(handle, "fasta"):
+                # make sure the sequence is lower case
+                if str(cur_record.seq).islower():
+                    dna_sequence = str(cur_record.seq).upper()
+                else:
+                    dna_sequence = str(cur_record.seq)
+                for i in range(0, len(dna_sequence), 3):
+                    codon = dna_sequence[i:i + 3]
+                    if codon in self.codon_count:
+                        self.codon_count[codon] += 1
+                    else:
+                        raise TypeError("illegal codon %s in gene: %s" % (codon, cur_record.id))
+
+    # this just gives the index when the objects is printed.
+    def print_index(self):
+        """Prints out the index used."""
+        for i in sorted(self.index):
+            print("%s\t%.3f" % (i, self.index[i]))
+
+
+
+
---
+++ Random_forest/libs/functions.py
@@ -0,0 +1,267 @@
+import datetime
+import sys
+import RNA
+import pandas as pd
+import numpy as np
+from libs import data
+from numpy.random import choice
+
+
+def progress(iteration, total):   
+    bars_string = int(float(iteration) / float(total) * 50.)
+    sys.stdout.write(
+        "\r|%-50s| %d%% (%s/%s)" % (
+            '█'*bars_string+ "░" * (50 - bars_string), float(iteration) / float(total) * 100,
+            iteration,
+            total
+        ) 
+    )
+    sys.stdout.flush()
+    if iteration == total:
+        print(' Completed!') 
+
+
+def sequence_length(seq):
+    '''
+    returns length of sequence in multiple of 3 by chopping extra positions
+    '''
+    if len(seq)%3 != 0:
+        length = (len(seq)- len(seq)%3)
+    else:
+        length = len(seq)
+    return length
+        
+def splitter(sequence,length):
+    '''
+    split sequence to codons
+    '''
+    length = length - length%3
+    if length == 0:
+        sys.stderr.write("Too small sequence. Trying for 3 nucleotides.\r")
+        sys.stdout.flush()
+        length = 3
+    split_func = lambda sequence, n: [sequence[i:i+n] for\
+                                    i in range(0, length, n)]
+    return split_func(sequence,3)
+
+
+def fasta_to_dataframe(input_file):
+    '''
+    convert fasta to pandas dataframe
+    '''
+    sequence_df = pd.DataFrame(columns=[1,0])
+    fasta = []
+    test = []
+    with open(input_file) as file:
+        for line in file:
+            line = line.strip()
+            if not line:
+               continue
+            if line.startswith(">"):
+                active_sequence_name = line[1:]
+                sequence_df.loc[line,1] = line[1:]
+                if active_sequence_name not in fasta:
+                    test.append(''.join(fasta))
+                    fasta = []
+                continue
+            sequence = line
+            fasta.append(sequence)
+    if fasta:
+        test.append(''.join(fasta))
+    for i, row in enumerate(test):
+        sequence_df[0][i-1] = row
+    return(sequence_df)
+
+
+
+
+def positionwise_codons(sequences,length_to_train,trainall=False):
+    '''
+    returns dictionary of positions(index) with codons.
+    this will be converted to pandas dataframe soon
+    ***deprecated. use codons_to_df**
+    '''
+    codon_dict={} 
+    for sequence in sequences:
+        length = sequence_length(sequence) if trainall==True or \
+                 len(sequence)<length_to_train else length_to_train 
+        codon_list = splitter(sequence.lower(),length)
+        for i in range(len(codon_list)):
+            try:
+                codon_dict.setdefault(i, [])
+                codon_dict[i].append(codon_list[i])
+            except KeyError:
+                codon_dict[i] = codon_list[i]
+
+    return codon_dict
+
+def codons_to_df(sequences,length_to_train,trainall=False):
+    '''
+    returns a dataframe of codons in given sequences
+    position is column and sequence number is index
+    '''
+    codon_df = pd.DataFrame()
+    index = 0
+    skipped = 0
+    for sequence in sequences:
+        try:
+            length = sequence_length(sequence) if trainall==True or \
+                     len(sequence)<length_to_train else length_to_train
+        except TypeError:
+            print("Something's wrong with the input sequences.\n")
+            break
+                  
+        codon_list = splitter(sequence.lower().replace('u','t'),length)
+        stop = ['tag','taa','tga']
+        if bool(set(stop).intersection(codon_list[:len(codon_list)-1])) == False:
+            for i in range(len(codon_list)):
+                codon_df.at[index,i]=codon_list[i]
+            
+        else:
+            print('\nStop codon encountered somewhere before the last position.')
+            skipped += 1
+        progress(index,len(sequences)-1)
+        index += 1
+    if skipped > 0:
+        print(skipped, 'sequence(s) were skipped because we encountered stop codons.')
+    
+
+    return codon_df
+
+
+def calc_cond_prob(codon_df):
+    '''
+    returns a first order conditional probability of codons
+    '''
+    cond_prob_df = []
+    for i in range(len(codon_df.columns)-1): #conditional except last one
+        cond_prob_df.append((codon_df.groupby([i, i+1]).count()\
+                             / codon_df.groupby(i).count()))
+        progress(i,len(codon_df.columns)-2)
+    return cond_prob_df
+
+
+
+    
+def train(codon_df):
+    '''
+    uses the codon dataframe to calculate probabilities
+    index is the codon name and column number is the position
+    '''
+    codons = [key for key,value in data.codon2aa.items()]
+    prob_df = pd.DataFrame(index=[codons])
+    for i in range(len(codon_df.columns)):
+        prob_df[i]=np.nan*len(prob_df)
+        probs=codon_df.groupby(i).size().\
+                                      div(codon_df[i].count())
+        for item in codon_df[i]:
+            if str(item)!= 'nan':
+                prob_df[i].loc[item]=probs.loc[item]
+        progress(i,len(codon_df.columns)-1)    
+    return prob_df
+
+
+def score(sequence_df,prob_data,back_prob_data):
+    '''
+    bitscore a list of sequences using a foreground and a background model
+    '''
+    sequence_score=pd.DataFrame(columns=['scores'],index=list(range(len(sequence_df))))
+    length_to_score = max([len(sequence_df[0][i]) for i in range(len(sequence_df))]) #score for full length
+    for seq in range(len(sequence_df)):
+        sequence = sequence_df[0][seq].lower()
+        length = sequence_length(sequence) if len(sequence)<length_to_score\
+                 else length_to_score
+        codons = splitter(sequence,length)
+        scores_df = pd.DataFrame(columns=['scores'],index=codons)
+        back_scores_df = pd.DataFrame(columns=['scores'],index=codons)
+        stop = ['tag','taa','tga']
+        if bool(set(stop).intersection(codons[:len(codons)-1])) == False:
+            scores_df = pd.DataFrame(columns=['scores'],index=codons)
+            back_scores_df = pd.DataFrame(columns=['scores'],index=codons)
+            for i in range(len(codons)):
+                try:
+                    if i < len(prob_data.columns)-1: #1 for average
+                        scores_df.loc[codons[i],'scores'] = np.log2(prob_data.loc[codons[i],i])[0]
+                        back_scores_df.loc[codons[i],'scores'] = np.log2(back_prob_data.loc[codons[i],i])[0]
+                    else:
+                        scores_df.loc[codons[i],'scores'] = np.log2(prob_data.loc[codons[i],'codon_prob'])[0]
+                        back_scores_df.loc[codons[i],'scores'] = np.log2(back_prob_data.loc[codons[i],'codon_prob'])[0]
+
+
+                except RuntimeWarning: #catch for log zero error
+                    scores_df.loc[codons[i],'scores'] = np.log2(prob_data.loc[codons[i],'codon_prob'])[0]
+                    back_scores_df.loc[codons[i],'scores'] = np.log2(back_prob_data.loc[codons[i],'codon_prob'])[0]
+        else:
+            print('\nStop codons encountered before the last position for sequence : ', seq)
+            pass
+
+        sequence_score.loc[seq,'scores'] = scores_df.sum()[0] - back_scores_df.sum()[0] 
+        progress(seq,len(sequence_df))
+        
+    return sequence_score
+
+
+
+
+def rna_ss(seq):
+    '''calculates minimum free energy of sequence using RNAlib
+    '''
+    ss,mfe = RNA.fold(seq)
+    return mfe
+
+
+def substitute_codon(sequence,prob_df,length_to_count = 30,subst = 2):
+    '''synonymously mutate sequence. The probability of codon from the model is used as a 
+    weght to randomly pick the new codon. 
+    length_to_count = length to consider for substitution. Default = 10 codons
+    subst = max number of codons to substitute. Default = 2 substitutions
+    '''
+    length = sequence_length(sequence) if len(sequence)<length_to_count else length_to_count
+    new_seq = sequence
+    for i in range(subst):
+        codons = functions.splitter(new_seq,length)
+        subst_codon_position = choice(list(range(len(codons)))) #randomly choose pos to mutate
+        subst_synonymous_codons = data.aa2codon[data.codon2aa[codons[subst_codon_position]]]
+
+        #with probs of codon as weights for random picking
+        try:
+            probs_of_codons_for_subst = prob_df.loc[subst_synonymous_codons,subst_codon_position]
+        except IndexError:
+            probs_of_codons_for_subst = prob_df.loc[subst_synonymous_codons,'codon_prob']
+
+        probs_sum = sum(probs_of_codons_for_subst)
+        subst_codon = choice(subst_synonymous_codons, p = [float(i)/probs_sum\
+                                                            for i in probs_of_codons_for_subst])
+
+
+        #without weights
+        #mutated_codon = random.choice(mutable_synonymous_codons)
+        new_seq = new_seq[:subst_codon_position*3]+ subst_codon + sequence[subst_codon_position*3+3:]
+        
+        
+    return new_seq
+
+
+def sim_anneal(sequence,prob_df,length = 30,niter=100):
+    '''
+    preforms a simulated annealing to maximize the mfe of sequence
+    '''
+    utr='ggggaattgtgagcggataacaattcccctctagaaataattttgtttaactttaagaaggagatatacat'
+    seq = sequence[:length] 
+    temp = np.linspace(1,0.001,niter)
+    scurr = seq
+    sbest = seq
+    for i in range(niter):
+        T = temp[i]
+        snew = substitute_codon(sbest,prob_df,length)[:length]
+        if rna_ss(utr[-length:]+ snew) >= rna_ss(utr[-length:] + scurr):
+                scurr = snew
+                if rna_ss(utr[-length:]+scurr)>=rna_ss(utr[-length:]+sbest):
+                    sbest = snew
+        elif np.exp(-(rna_ss(utr[-length:]+scurr)-rna_ss(utr[-length:]+snew))/T) <= np.random.rand(1)[0]:
+            scurr = snew
+        functions.progress(i,niter)
+    print('\nmfe is',rna_ss(utr[-length:]+sbest),sbest)
+    annealed_seq = sbest + sequence[length:]
+    return annealed_seq
+
