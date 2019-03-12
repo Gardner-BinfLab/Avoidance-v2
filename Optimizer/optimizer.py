77227cedf8aa810522354f4d8cbac4c2e8ae01b8
Bikash Kumar Bhandari <30164638+bkb3@users.noreply.github.com>
2019-03-12 23:09:47 +1300

    Update optimizer.py

--- Optimizer/optimizer.py
+++ Optimizer/optimizer.py
@@ -4,6 +4,8 @@ import argparse
 import time
 import pandas as pd
 import numpy as np
+import pickle
+from os import path
 from libs import functions,data,features
 #from functools import partial
 
@@ -19,31 +21,30 @@ def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Training script')
     parser.add_argument('-m', '--mrna',
                         type=valid_file,
-                        help='mrna sequence to optimize in csv or fasta.',
+                        help='mrna sequence from hmm in csv or fasta.',
                         required='True')
+    parser.add_argument('-r', '--randomforest',
+                        help='random forest model',
+                        required='True')  
     parser.add_argument('-o', '--output',
                         help='Output file name.',
                         default = 'optimized_sequences')
 
     results = parser.parse_args(args)
     return (results.mrna,
+            results.randomforest,
             results.output)
 
 
-def optimization(sequence):
-    optimization = features.Optimize(sequence,cai_mean, cai_std,\
-                                     gc_cont_mean,gc_cont_std,ss_mean,\
-                                     ss_std, avd_mean, avd_std,1000)
-    optimized_sequence = [optimization.simulated_anneal() \
-                          for _ in range(2)]
-    result_df = pd.DataFrame({sequence:optimized_sequence})
-
-    return result_df
-
-
-def dummy_func(optimization_df):
-    optimization_df['annealed'] = optimization_df['objects'].apply(lambda x:x.simulated_anneal())
-    return optimization_df
+def all_features(dataframe):
+    dataframe['analyze'] =  dataframe['sequence'].apply(lambda x:features.Analyze(x))
+    dataframe['accs'] = dataframe['analyze'].apply(lambda x:x.access_calc())
+    dataframe['sec_str'] = dataframe['analyze'].apply(lambda x:x.sec_str())
+    dataframe['cai'] = dataframe['analyze'].apply(lambda x:x.cai())
+    dataframe['gc_cont'] = dataframe['analyze'].apply(lambda x:x.gc_cont())
+    dataframe['avd'] = dataframe['analyze'].apply(lambda x:x.avoidance())
+    
+    return dataframe
 
 
 
@@ -61,32 +62,53 @@ def main():
     if ext.lower() in ('.fasta','.fa'):
         mrna_df = functions.read_fasta(m)
     else:
-        mrna_df = pd.read_csv(m)        
+        mrna_df = pd.read_csv(m) 
         
-    backgnd_seq = functions.background(mrna_df['sequence'][0],10) 
+    #first we find good sequences from the given list via random forest
+    #then those good sequences can be optimized further by simulated annealing
     
-    #calculate features for the background sequences first
-    print('1) calculating features for background sequences..', flush = True)
-    backgnd_seq['features'] = backgnd_seq['sequence'].apply(lambda x:\
-                                                            features.Analyze(x))
+    #for mrna scoring and selecting
+    print('calculating features for given sequence list..', flush=True)
+    mrna_df = all_features(mrna_df)
+    print('done!',flush=True)
+    
+    ##for random forest
+    #load pre-trained rf
+    print('loading random forest model..',flush=True)
+    rf_model = pickle.load(open(r, 'rb'))
+    print('done!',flush=True)
+    
+    
+    print('selecting good sequences..', flush=True)
+    mrna_df['rf_input'] = [mrna_df[['accs','sec_str','cai','gc_cont','avd']].values[x]\
+                           for x in range(mrna_df.shape[0])]
+    #we keep threshold of 0.9 i.e.. anything above or equal to 0.9 is 1, rest are 0
+    mrna_df['rf_results'] = mrna_df['rf_input'].apply(lambda x: 1 if \
+                                                      rf_model.predict_proba([x])[0][1] >=0.9 else 0)
+    
+    #pick those sequences with 1 from Random forest
+    choosen_seq = mrna_df.loc[(mrna_df['rf_results'] == 1)].reset_index(drop=True)
+    if choosen_seq.shape[0] == 0:
+        print('failed to find any good sequences at this stage..',flush=True)
+        print('optimizing all of provided sequence instead..', flush=True)
+        choosen_seq = mrna_df
+    else:
+        print('done!',flush=True)
+    
+    
+    ##Now we generate 1000 random synonymous variants as a background
+    #check if we already have background sequences and features
+    if path.exists('background_sequences.csv'):
+        print('previous background found! using it..', flush=True)
+        backgnd_seq=pd.read_csv('background_sequences.csv')
+    else:
+        print('generating 1000 random synonymous sequences..', flush=True)
+        backgnd_seq = functions.syn_background(mrna_df['sequence'][0],1000)
 
-    print('cai        ',end='\r',flush=True)
-    backgnd_seq['cai'] = backgnd_seq['features'].apply(lambda x : x.cai())
-    print('cai done!   ',end='\r',flush=True)
-    
-    print('gc_cont      ',end='\r',flush=True)
-    backgnd_seq['gc_cont'] = backgnd_seq['features'].apply(lambda x : x.gc_cont())
-    print('gc_cont done!',end='\r',flush=True)
-    
-    print('sec_str      ',end='\r',flush=True)
-    backgnd_seq['sec_str'] = backgnd_seq['features'].apply(lambda x : x.sec_str())
-    print('sec_str done!',end='\r',flush=True)
-    
-    print('avoidance     ',end='\r',flush=True)
-    backgnd_seq['avoidance'] = backgnd_seq['features'].apply(lambda x :\
-                                                             x.avoidance())
-    print('avoidance done!',end='\r',flush=True)
-    print('2) running optimizations. this may take a while...')
+        #calculate features for background
+        print('calculating features for background sequences..', flush=True)
+        backgnd_seq = all_features(backgnd_seq)
+        backgnd_seq.to_csv('background_sequences.csv',index=False)
     
     
     #for z scores
@@ -96,42 +118,52 @@ def main():
                                 np.std(backgnd_seq['gc_cont'].values)
     ss_mean, ss_std = np.mean(backgnd_seq['sec_str'].values),\
                       np.std(backgnd_seq['sec_str'].values)
-    avd_mean, avd_std = np.mean(backgnd_seq['avoidance'].values),\
-                      np.std(backgnd_seq['avoidance'].values)
+    avd_mean, avd_std = np.mean(backgnd_seq['avd'].values),\
+                      np.std(backgnd_seq['avd'].values)
+    accs_mean, accs_std = np.mean(backgnd_seq['accs'].values),\
+                      np.std(backgnd_seq['accs'].values) 
     
     
-
+    #optimization begins
+    print('optimization started..this may take a while..', flush=True)
     count = 0
-    opt_df = pd.DataFrame()
-    for sequence in mrna_df['sequence'] :
+    new_sequences = []
+    for sequence in choosen_seq['sequence'] :
         message='at sequence :'+ str(count)
-        functions.progress(count,mrna_df.shape[0],message)
+        functions.progress(count,choosen_seq.shape[0],message)
         
         optimization = features.Optimize(sequence,cai_mean, cai_std,gc_cont_mean,\
-                 gc_cont_std,ss_mean, ss_std, avd_mean, avd_std,2000)
-        sequence_count = mrna_df.loc[mrna_df['sequence']\
-                                                       == sequence].index[0]
+                 gc_cont_std,ss_mean, ss_std, avd_mean, avd_std,accs_mean,accs_std,1000)
         
         optimized_sequence = [optimization.simulated_anneal() \
-                              for _ in range(10)]
-        opt_df[sequence_count]=optimized_sequence
+                              for _ in range(10)] #10 new_sequences per given sequence
+        
+        new_sequences.append(optimized_sequence)
         count+=1
         message='at sequence :'+ str(count)
-        functions.progress(count,mrna_df.shape[0],message)
+        functions.progress(count,choosen_seq.shape[0],message)
         
-
-        
+    
+    #wew! finally we reached at the end
+    print('optimization completed! exporting sequences...',flush=True)    
+    final_sequences = pd.DataFrame({'sequence':[seq for sublist in new_sequences\
+                                                for seq in sublist]})
+    
     filename = mypath + o +'_'+time.strftime("%Y%m%d-%H%M%S")+'.csv'
-    opt_df.to_csv(filename,sep=',', encoding='utf-8', index=False)
+    final_sequences.to_csv(filename,sep=',', encoding='utf-8', index=False)
+    print('full process was completed successfully!', flush = True)
     
-    
+                                    
 
-    
-                   
-        
-        
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    m,o= check_arg(sys.argv[1:])
+    m,r,o= check_arg(sys.argv[1:])
     main()
         
